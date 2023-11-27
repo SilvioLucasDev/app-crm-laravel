d1e53133ce75e55692c859a9556499d07f81d2b1
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-11-27 01:27:22 -0300

    chore: add livewire, tailwindcss and maryui CRM-1

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./app/View/Components/**/**/*.php",
+        "./app/Livewire/**/**/*.php",
+        "./vendor/robsontenorio/mary/src/View/Components/**/*.php"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        daisyui
+    ],
+}
