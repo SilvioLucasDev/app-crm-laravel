25d251b4d72585b2cc7a7c4ea58c065784f16132
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-11-28 13:36:39 -0300

    feat: add password recovery route CRM-4

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ use Illuminate\Support\Facades\{Auth, Route};
 Route::get('/login', Login::class)->name('auth.login');
 Route::get('/register', Register::class)->name('auth.register');
 Route::get('/logout', fn () => Auth::logout())->name('auth.logout');
+Route::get('/password/recovery', fn () => 'Ola')->name('auth.password.recovery');
 
 Route::middleware('auth')->group(function () {
     Route::get('/', Welcome::class)->name('dashboard');
