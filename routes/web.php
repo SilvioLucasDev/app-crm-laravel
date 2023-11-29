2d017ab8f928babfe52dbbd528bea2524ad8b292
Silvio Lucas <silviolucas_santos@hotmail.com>
2023-11-29 02:46:13 -0300

    feat: should block the access to admin page if the user does not have the permission to be an admin CRM-5

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,25 @@ use Illuminate\Support\Facades\{Auth, Route};
 |
 */
 
+/**
+ * Auth Flow Routes
+ */
 Route::get('/login', Login::class)->name('auth.login');
 Route::get('/register', Register::class)->name('auth.register');
 Route::get('/logout', fn () => Auth::logout())->name('auth.logout');
 Route::get('/password/recovery', Password\Recovery::class)->name('auth.password.recovery');
 Route::get('/password/reset', Password\Reset::class)->name('password.reset');
 
+/**
+ * Authenticated Routes
+ */
 Route::middleware('auth')->group(function () {
     Route::get('/', Welcome::class)->name('dashboard');
+
+    /**
+     * Admin Routes
+     */
+    Route::prefix('/admin')->middleware('can:be-an-admin')->group(function () {
+        Route::get('/dashboard', fn () => 'Admin Dashboard')->name('admin.dashboard');
+    });
 });
