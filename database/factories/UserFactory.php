a6232a61b73a2d8db7ba68d84dfac0b9763df99f
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-12 18:33:20 -0300

    feat: let's create a livewire component to list as customers in the page CRM-25

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,7 +27,7 @@ class UserFactory extends Factory
             'email'             => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'validation_code'   => null,
-            'password'          => static::$password ??= Hash::make('password'),
+            'password'          => Hash::make('password'),
             'remember_token'    => Str::random(10),
         ];
     }
--- tests/Feature/Customer/ListCustomersTest.php
+++ tests/Feature/Customer/ListCustomersTest.php
@@ -1,6 +1,9 @@
 <?php
 
-use App\Models\User;
+use App\Livewire\Customers;
+use App\Models\{Customer, User};
+use Illuminate\Pagination\LengthAwarePaginator;
+use Livewire\Livewire;
 
 use function Pest\Laravel\{actingAs, get};
 
@@ -11,4 +14,26 @@ it('should be able to access the route customers', function () {
 
     get(route('customers'))
         ->assertOk();
+});
+
+it("let's create a livewire component to list as customers in the page", function () {
+    /** @var User $user */
+    $user = User::factory()->create();
+    actingAs($user);
+
+    $customers = Customer::factory(10)->create();
+
+    $livewire = Livewire::test(Customers\Index::class);
+
+    $livewire->assertSet('customers', function ($customers) {
+        expect($customers)
+            ->toBeInstanceOf(LengthAwarePaginator::class)
+            ->toHaveCount(10);
+
+        return true;
+    });
+
+    foreach ($customers as $customer) {
+        $livewire->assertSee($customer->name);
+    }
 });
