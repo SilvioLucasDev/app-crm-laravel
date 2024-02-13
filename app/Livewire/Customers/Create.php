a4aa6535b71720b29d316737fe2ad838fd6dc90a
SilvioLucasDev-OM30 <silvio.santos@om30tech.com.br>
2024-02-13 14:33:49 -0300

    feat: add modal to create new customer CRM-26

--- app/Livewire/Customers/Create.php
+++ app/Livewire/Customers/Create.php
@@ -4,11 +4,13 @@ namespace App\Livewire\Customers;
 
 use App\Models\Customer;
 use Illuminate\Contracts\View\View;
-use Livewire\Attributes\Rule;
+use Livewire\Attributes\{On, Rule};
 use Livewire\Component;
 
 class Create extends Component
 {
+    public bool $modal = false;
+
     #[Rule(['required', 'min:3', 'max:255'])]
     public string $name = '';
 
@@ -23,6 +25,13 @@ class Create extends Component
         return view('livewire.customers.create');
     }
 
+    #[On('customer::create')]
+    public function openModal(): void
+    {
+        $this->resetErrorBag();
+        $this->modal = true;
+    }
+
     public function save(): void
     {
         $this->validate();
@@ -33,5 +42,7 @@ class Create extends Component
             'email' => $this->email,
             'phone' => $this->phone,
         ]);
+
+        $this->modal = false;
     }
 }
--- resources/views/livewire/customers/create.blade.php
+++ resources/views/livewire/customers/create.blade.php
@@ -1,3 +1,19 @@
 <div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+    <x-drawer wire:model="modal" class="w-1/3" right>
+        <x-form wire:submit="save" id="create-customer-form">
+            <x-card title="Create Customer" separator>
+                <div class="space-y-2">
+                    <x-input label="Name" wire:model="name" />
+                    <x-input label="Email" wire:model="email" />
+                    <x-input label="Phone" wire:model="phone" />
+                </div>
+
+                <x-slot:actions>
+                    <x-button label="Cancel" @click="$wire.modal = false" />
+                    <x-button label="Save" type="submit" class="btn-md btn-primary" form="create-customer-form"
+                        spinner="save" />
+                </x-slot:actions>
+            </x-card>
+        </x-form>
+    </x-drawer>
 </div>
