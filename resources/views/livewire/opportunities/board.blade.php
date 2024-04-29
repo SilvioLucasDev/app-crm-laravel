61bcf3a80718d3573116844764fcd511f5fb5bbb
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-04-29 20:13:29 -0300

    feat: list card of database CRM-32 CRM-32

--- resources/views/livewire/opportunities/board.blade.php
+++ resources/views/livewire/opportunities/board.blade.php
@@ -1,31 +1,14 @@
 <div class="grid grid-cols-3 gap-4 h-full">
-    <div class="bg-base-200 p-2 rounded-md">
-        <x-header class="px-2 mb-0" size="text-xl" title="Open" subtitle="Total 2 opportunities" separator
-            progress-indicator />
+    @foreach ($this->opportunities as $status => $items)
+        <div class="bg-base-200 p-2 rounded-md">
+            <x-header :title="$status" subtitle="Total {{ $items->count() }} opportunities" class="px-2 mb-0"
+                size="text-xl" separator progress-indicator />
 
-        <div class="space-y-2 px-2">
-            <x-card>OPP 1</x-card>
-            <x-card>OPP 2</x-card>
+            <div class="space-y-2 px-2">
+                @foreach ($items as $item)
+                    <x-card>{{ $item->title }}</x-card>
+                @endforeach
+            </div>
         </div>
-    </div>
-
-    <div class="bg-base-200 p-2 rounded-md">
-        <x-header class="px-2 mb-0" size="text-xl" title="Won" subtitle="Total 2 opportunities" separator
-            progress-indicator />
-
-        <div class="space-y-2 px-2">
-            <x-card>OPP 1</x-card>
-            <x-card>OPP 2</x-card>
-        </div>
-    </div>
-
-    <div class="bg-base-200 p-2 rounded-md">
-        <x-header class="px-2 mb-0" size="text-xl" title="Lost" subtitle="Total 2 opportunities" separator
-            progress-indicator />
-
-        <div class="space-y-2 px-2">
-            <x-card>OPP 1</x-card>
-            <x-card>OPP 2</x-card>
-        </div>
-    </div>
+    @endforeach
 </div>
