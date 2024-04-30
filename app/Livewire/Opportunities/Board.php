3ec873314cc4ddf3975ab1809f95ffd907457e03
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-04-29 21:33:45 -0300

    feat: add logic in Board component CRM-32 CRM-32

--- app/Livewire/Opportunities/Board.php
+++ app/Livewire/Opportunities/Board.php
@@ -5,6 +5,7 @@ namespace App\Livewire\Opportunities;
 use App\Models\Opportunity;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Facades\DB;
 use Livewire\Attributes\Computed;
 use Livewire\Component;
 
@@ -19,19 +20,33 @@ class Board extends Component
     public function opportunities(): Collection
     {
         return Opportunity::query()
-            ->orderByRaw("
-                case
-                    when status = 'open' then 1
-                    when status = 'won' then 2
-                    when status = 'lost' then 3
-                end
-            ")
+            ->orderByRaw("field(status, 'open', 'won', 'lost')")
+            ->orderBy('sort_order')
             ->get()
             ->groupBy('status');
     }
 
     public function updateOpportunities($data)
     {
+        $order = collect();
 
+        foreach ($data as $group) {
+            $items = collect($group['items'])
+                ->map(fn ($item) => $item['value'])
+                ->join(',');
+
+            $order->push($items);
+        }
+
+        $open = explode(',', $order[0]);
+        $won  = explode(',', $order[1]);
+        $lost = explode(',', $order[2]);
+
+        $sortOrder = $order->join(',');
+
+        Opportunity::query()->whereIn('id', $open)->update(['status' => 'open']);
+        Opportunity::query()->whereIn('id', $won)->update(['status' => 'won']);
+        Opportunity::query()->whereIn('id', $lost)->update(['status' => 'lost']);
+        Opportunity::query()->update(['sort_order' => DB::raw("field(id, $sortOrder)")]);
     }
 }
