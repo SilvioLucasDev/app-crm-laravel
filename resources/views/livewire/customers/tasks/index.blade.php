3b24fd19607ffea4d707ecb9a5e400599efb7235
“SilvoLucasDev” <silviolucas_santos@hotmail.com>
2024-10-23 01:58:53 -0300

    feat: add logic to hide and show done tasks CRM-35

--- resources/views/livewire/customers/tasks/index.blade.php
+++ resources/views/livewire/customers/tasks/index.blade.php
@@ -3,9 +3,9 @@
 
     <hr class="border-dashed border-gray-700 my-4">
 
-    <h2 class="uppercase font-bold text-gray-600 text-xs mb-2">
+    <div class="uppercase font-bold text-gray-600 text-xs mb-2">
         Pending [{{ $this->notDoneTasks->count() }}]
-    </h2>
+    </div>
 
     <ul class="flex flex-col gap-2" wire:sortable="updateTaskOrder" wire:sortable.options="{ animation: 100 }">
         @foreach ($this->notDoneTasks as $task)
@@ -17,30 +17,37 @@
 
     <hr class="border-dashed border-gray-700 my-4">
 
-    <h2 class="uppercase font-bold text-gray-600 text-xs mb-2">
-        Done tasks [{{ $this->doneTasks->count() }}]
-    </h2>
+    <div x-data="{ show: true }">
+        <div class="uppercase font-bold text-gray-600 text-xs mb-2 flex items-center gap-2 justify-between">
+            <span>Done [{{ $this->doneTasks->count() }}]</span>
 
-    <ul class="flex flex-col gap-2">
-        @foreach ($this->doneTasks as $task)
-            <li class="flex items-start gap-2 justify-between">
-                <div class="flex gap-2 items-center">
-                    <input id="task-{{ $task->id }}" type="checkbox" value="1" wire:click="toggleCheck({{ $task }}, 'pending' )" @if ($task->done_at) checked @endif />
+            <button title="Hide/Show list" @click="show = !show" type="button">
+                <x-icon x-show="!show" name="o-chevron-up" class="w-4 h-4 -mt-1 opacity-50 hover:opacity-100 hover:text-primary" />
+                <x-icon x-show="show" name="o-chevron-down" class="w-4 h-4 -mt-1 opacity-50 hover:opacity-100 hover:text-primary" />
+            </button>
+        </div>
 
-                    <label for="task-{{ $task->id }}">
-                        {{ $task->title }}
-                    </label>
+        <ul class="flex flex-col gap-2" x-show="show" x-transition>
+            @foreach ($this->doneTasks as $task)
+                <li class="flex items-start gap-2 justify-between">
+                    <div class="flex gap-2 items-center">
+                        <input id="task-{{ $task->id }}" type="checkbox" value="1" wire:click="toggleCheck({{ $task }}, 'pending' )" @if ($task->done_at) checked @endif />
 
-                    @if ($task->assignedTo)
-                        <div class="text-xs italic opacity-30">
-                            Assigned to: {{ $task->assignedTo->name }}
-                        </div>
-                    @endif
-                </div>
-                <button title="Delete task" wire:click="deleteTask({{ $task }})">
-                    <x-icon name="o-trash" class="w-4 h-4 -mt-1 opacity-30 hover:opacity-100 hover:text-error" />
-                </button>
-            </li>
-        @endforeach
-    </ul>
+                        <label for="task-{{ $task->id }}">
+                            {{ $task->title }}
+                        </label>
+
+                        @if ($task->assignedTo)
+                            <div class="text-xs italic opacity-30">
+                                Assigned to: {{ $task->assignedTo->name }}
+                            </div>
+                        @endif
+                    </div>
+                    <button title="Delete task" wire:click="deleteTask({{ $task }})">
+                        <x-icon name="o-trash" class="w-4 h-4 -mt-1 opacity-30 hover:opacity-100 hover:text-error" />
+                    </button>
+                </li>
+            @endforeach
+        </ul>
+    </div>
 </div>
